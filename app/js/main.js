4bfa21e93a5c983c1816b18019057243924f379e
tonycorejz <mr.korovin-mihail@ya.ru>
2020-03-26 19:18:45 +0700

    Сделал набросок первой секции

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,6 @@
-$(function(){
-
-
-
-  
+$("#navToggle").click(function() {
+    $(this).toggleClass("active");
+    $(".overlay").toggleClass("open");
+    // this line ▼ prevents content scroll-behind
+    $("body").toggleClass("locked"); 
 });
